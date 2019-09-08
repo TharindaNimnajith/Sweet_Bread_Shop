15d7e53b4ee6fce702e47c3207eb57565facf538
SethmaWattegedara <sethlewa@gmail.com>
2019-09-08 20:28:24 +0530

    create register ui

---
+++ app/src/main/java/com/example/ebreadshop/user/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.ebreadshop.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ebreadshop.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
---
+++ app/src/main/java/com/example/ebreadshop/user/custHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.ebreadshop.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ebreadshop.R;
+
+public class custHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_home);
+    }
+}
