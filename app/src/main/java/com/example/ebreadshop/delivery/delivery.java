6e9c6c841565d8c4b2a66c82387661effecdacca
ANgajasinghe <angajasinghe@gmail.com>
2019-09-09 21:04:17 +0530

    dd

--- app/src/main/java/com/example/ebreadshop/delivery/delivery.java
+++ app/src/main/java/com/example/ebreadshop/delivery/delivery.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.ebreadshop.R;
 
-public class MainActivity extends AppCompatActivity {
+public class delivery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_delivery);
     }
 }
