392d7920cc52a722a2c8f553dc08c065b1228c29
TharindaNimnajith <tharindarajapakshe@gmail.com>
2019-09-09 21:19:15 +0530

    Revert "dd"
    This reverts commit 6e9c6c84

--- app/src/main/java/com/example/ebreadshop/delivery/MainActivity.java
+++ app/src/main/java/com/example/ebreadshop/delivery/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ebreadshop.delivery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ebreadshop.R;
 
