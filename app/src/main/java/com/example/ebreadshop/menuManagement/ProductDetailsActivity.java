7c90825e4f52ece2c55cfa783d03c77e474ac966
kavindi97 <42095172+kavindi97@users.noreply.github.com>
2019-09-21 10:45:23 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/ebreadshop/menuManagement/ProductDetailsActivity.java
@@ -0,0 +1,68 @@
+package com.example.ebreadshop.menuManagement;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.ebreadshop.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    private Button addToCartButton;
+    private ImageView productImage;
+    private ElegantNumberButton numberButton;
+    private TextView productPrice,productDescription,productName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+
+        addToCartButton= (Button) findViewById(R.id.pd_add_to_cart_button)
+        numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
+        productImage = (ImageView) findViewById(R.id.product_image_details);
+        productName = (TextView) findViewById(R.id.product_name_details);
+        productDescription = (TextView) findViewById(R.id.product_description_detail);
+        productPrice = (TextView) findViewById(R.id.product_price_detail);
+
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addingToCartList();
+            }
+        });
+    }
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calForDate =   Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+        DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("pname", productName.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberButton.getNumber());
+        cartMap.put("discount","");
+
+
+    }
+}
