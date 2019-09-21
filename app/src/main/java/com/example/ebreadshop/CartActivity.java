d9902cdda2ad3387c83edcf4f837ef1c9f543754
kavindi <uldkavindigunasinghe@gmail.com>
2019-09-21 13:45:45 +0530

    cart list

---
+++ app/src/main/java/com/example/ebreadshop/CartActivity.java
@@ -0,0 +1,33 @@
+package com.example.ebreadshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
+        txtTotalAmount = (TextView) findViewById(R.id.total_price);
+    }
+}
